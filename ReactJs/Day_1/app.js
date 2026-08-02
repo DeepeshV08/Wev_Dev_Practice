ffc373a11522323b69920ff232577fa8fb74d032
Your Name <deepesh3950lodhi@gmail.com>
2026-08-02 09:40:30 +0530

    Backend

---
+++ ReactJs/Day_1/app.js
@@ -0,0 +1,8 @@
+export const a = 12  // named export 
+
+const user = "Deepesh"
+
+const age = 20
+
+// We can export default only once in a single file
+export default user
---
+++ ReactJs/Day_1/script.js
@@ -0,0 +1,5 @@
+import {a} from './app.js' 
+import name from './app.js'
+
+console.log(a)
+console.log(name)
